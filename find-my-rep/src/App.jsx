144d6ef4d391195af523138dba21bdd2c980f862
trastephenson <mrtravisstephenson@gmail.com>
2022-08-22 12:27:22 -0600

    Add files via upload

---
+++ find-my-rep/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/header/Header";
+import Selection from "./components/selection/Selection";
+
+
+
+
+const App = () => {
+  return (
+    <>
+        <Header />
+       
+        <Selection />
+       
+
+    </>  
+    
+  )
+}
+
+export default App;
---
+++ find-my-rep/src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './header.css'
+
+
+const Header = () => {
+    return (
+     <header>
+      <div className="container header__container">
+        <h1>Who's My Representative?</h1>
+        
+
+       
+      </div>
+     </header>
+    )
+  }
+  
+  export default Header
